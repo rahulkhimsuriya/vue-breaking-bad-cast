ef737ec3de13c824763c02240bc521c6ab93b542
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-07-10 11:17:40 +0530

    refactor code

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,41 @@
       <img class="w-40 h-auto" alt="Vue logo" src="./assets/image/logo.png" />
     </div>
 
-    <div class="container mx-auto mt-8">
-      <CharacterList></CharacterList>
+    <div class="container mx-auto">
+      <div class="flex flex-wrap justify-center md:justify-start">
+        <CharacterCard
+          v-for="character in characters"
+          :character="character"
+          :key="character.char_id"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import CharacterList from '@/components/CharacterList.vue'
+import axios from 'axios'
+
+import CharacterCard from '@/components/CharacterCard.vue'
 
 export default {
   name: 'App',
-  components: { CharacterList }
+  components: { CharacterCard },
+
+  data() {
+    return {
+      characters: []
+    }
+  },
+
+  created() {
+    axios
+      .get('https://breakingbadapi.com/api/characters')
+      .then(({ data }) => (this.characters = data))
+      .catch(err => console.log(err))
+  },
+
+  methods: {}
 }
 </script>
 
